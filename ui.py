60818cbd353d94d052f0435e4b0cb079a9434526
Saiyam-Sandhir <saiyamsandhir@gmail.com>
2023-01-29 17:17:45 +0530

    New Ui created using custom tkinter

--- ui.py
+++ ui.py
@@ -1,6 +1,8 @@
 #MIT License Copyright (c) 2023 Saiyam Jain
 
+import customtkinter as ctk
 import tkinter as tk
+from PIL import Image
 from quiz_brain import QuizBrain, new_quiz
 
 THEME_COLOR = "#375362"
@@ -11,28 +13,30 @@ class Ui():
 
         self.win = tk.Tk()
         self.win.title("Quizzler")
-        self.win.config(padx = 20, pady = 20, background = THEME_COLOR)
+        self.win.iconbitmap("images\\icon.ico")
+        self.win.resizable(False, False)
+        self.win.config(background = THEME_COLOR, padx = 10, pady = 10)
 
-        self.score_text = tk.Label(text= f"Score: {self.quiz.score}", font = ("Arial", 10), foreground = "white", background = THEME_COLOR)
+        self.score_text = ctk.CTkLabel(self.win, text= f"Score: {self.quiz.score}", font = ("Arial", 20), text_color = "white", fg_color = THEME_COLOR)
         self.score_text.grid(column = 1, sticky = "e", pady = (0, 20))
 
-        self.canvas = tk.Canvas(height = 400, width = 400, background = "white")
+        self.canvas = tk.Canvas(self.win, height = 400, width = 400, background = "white")
         self.canvas.grid(column = 0, row = 1, columnspan = 2)
 
         self.question_text = self.canvas.create_text(200, 200, text = "Questions here", font = ("Arial", 20, "italic"), fill = THEME_COLOR, width = 400)
         self.get_next_question()
 
-        restart_img = tk.PhotoImage(file = "images\\restart.png")
-        self.restart_button = tk.Button(image = restart_img, height = 97, width = 100, highlightthickness = 0, relief = "flat", command = self.restart_game)
+        restart_img = ctk.CTkImage(light_image = Image.open("images\\restart.png"), dark_image = Image.open("images\\restart.png"), size = (75, 75))
+        self.restart_button = ctk.CTkButton(self.win, image = restart_img, text = "", height = 97, width = 100, command = self.restart_game)
         self.restart_button.grid(column = 1, row = 2, pady = (20,0), columnspan = 2)
         self.restart_button.grid_forget()
 
-        true_img = tk.PhotoImage(file = "images\\true.png")
-        self.true_button = tk.Button(image = true_img, height = 97, width = 100, highlightthickness = 0, relief = "flat", command = lambda : self.get_next_question("True"))
+        true_img = ctk.CTkImage(light_image = Image.open("images\\true.png"), dark_image = Image.open("images\\true.png"), size = (75, 75))
+        self.true_button = ctk.CTkButton(self.win, image = true_img, text = "", height = 97, width = 100, command = lambda : self.get_next_question("True"))
         self.true_button.grid(column = 0, row = 2, pady = (20, 0))
 
-        false_img = tk.PhotoImage(file = "images\\false.png")
-        self.false_button = tk.Button(image = false_img, height = 97, width = 100, highlightthickness = 0, relief = "flat", command = lambda : self.get_next_question("False"))
+        false_img = ctk.CTkImage(light_image = Image.open("images\\false.png"), dark_image = Image.open("images\\false.png"), size = (75, 75))
+        self.false_button = ctk.CTkButton(self.win, image = false_img, text = "", height = 100, width = 100, command = lambda : self.get_next_question("False"))
         self.false_button.grid(column = 1, row = 2, pady = (20, 0))
 
         self.win.mainloop()
@@ -55,10 +59,10 @@ class Ui():
         correct_answer = self.quiz.current_question.answer
         if ans == correct_answer:
             self.quiz.score += 1
-            self.score_text.config(text = f"Score: {self.quiz.score}")
+            self.score_text.configure(text = f"Score: {self.quiz.score}")
 
     def restart_game(self):
-        self.score_text.config(text= "Score: 0")
+        self.score_text.configure(text= "Score: 0")
         self.score_text.grid(column = 1, row = 0, sticky = "e", pady = (0, 20))
         self.quiz = new_quiz()
         self.get_next_question()
